b2e08ecfe2e952b73178fa598ae6950521b69bf0
Akhireddy <akhir1989@gmail.com>
2024-07-09 12:09:16 +0530

    devops-practice

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+#Declearing a variable
 PERSON1=$1 # No space between person1 and name
 PERSON2=$2
 
+#refering a variable 
 echo "$PERSON1:: hello $PERSON2 , how are you?"
 echo "$PERSON2:: iam good $PERSON1 ,how about you?"
 echo "$PERSON1:: iam good too, how about work?"
---
+++ 04-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "please enter username::"
+read Username:: # here username is varible 
+echo "please enter password::"
+read password:: 
+echo "username is:$username, password is:$password"
