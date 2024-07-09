a3ee9d7f2c17fddc78fedb650a63491212640481
Akhireddy <akhir1989@gmail.com>
2024-07-09 15:18:59 +0530

    devops-practice

--- 07-special-variables.sh
+++ 07-special-variables.sh
@@ -8,5 +8,5 @@ echo "Current working directory: $PWD"
 echo "Home directory current user: $HOME"
 echo "Which user is running this script: $USER"
 echo " process ID of the current shell script: $$"
-Sleep 30 &
+sleep 30 &
 echo "process id of last background command: $!"
