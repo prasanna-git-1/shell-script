fc8994eed03d66e0797ce4b0eb4de690ca02c316
Akhireddy <akhir1989@gmail.com>
2024-07-09 15:30:59 +0530

    devops-practice

--- 08-coditions.sh
+++ 08-coditions.sh
@@ -2,11 +2,11 @@
  
  number=$1
 
- if [ $number -lt 10 ]
+ if [ $number -eq 10 ]
  then
       echo "given number is grater than 10 "
  else 
-       echo "given number is lee than 10"
+       echo "given number is less than 10"
   fi
 
   # -gt = greater than ,-lt = lessthan , -eq = equal to , -ge greater equal to ,-le = less than or equal to
