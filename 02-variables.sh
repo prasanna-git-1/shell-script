9b383cafe6ae01350ae583f62774cb69f1586126
Akhireddy <akhir1989@gmail.com>
2024-07-09 11:35:52 +0530

    devops-practice

---
+++ 02-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERON1=RAMESH # No space between person1 and name
+PERSON2=SURESH
+
+echo "$PERSON1:: hello $PERSON2 , how are you?"
+echo "$PERSON2:: iam good $PRESON1 ,how about you?"
+echo "$PERSON1:: iam good too, how about work?"
+echo "suresh:: good , hoe about your work?"
