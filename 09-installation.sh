983f848aa807140b9d0f53c20395a63e180acc81
Akhireddy <akhir1989@gmail.com>
2024-07-11 16:41:07 +0530

    practice

---
+++ 09-installation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+UESRID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+   echo "please run the script with super user"
+   exit 1 # manually we are stopping the process manually
+else
+    echo "you are a super user"
+fi 
+
+
+dnf install mysql -y 
